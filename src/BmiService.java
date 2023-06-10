a4ee6408b5f2ae19ecece6a8a37db1e1eda2f894
natalikazakova <natalikazakova87@gmail.com>
2023-06-10 11:50:59 +0700

    initial commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,6 +2,6 @@ public class BmiService {
     public int calculate(double height, int weight) {
         int result;
         result = (int) ((int) weight / (height * height));
-    return result;
+        return result;
     }
 }
